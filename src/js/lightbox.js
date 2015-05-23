a22c2645c985a79a148d748c609376474db0a0cf
@zip <->
2015-05-23 15:40:32 -0700

    Generalised some more

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,12 +1,15 @@
 'use strict';
 
 window.Lightbox = (function () {
+  var prefix = "zip-";
+
   function View(element) {
     if (!(this instanceof View)) {
       throw new Error("Called a View() or subclass without new");
     }
     var self = this;
     this.element = element;
+    element.view = this;
 
     // Fire an event every time a mutation occurs
     var observer = new MutationObserver(function (mutations) {
@@ -21,66 +24,50 @@ window.Lightbox = (function () {
   View.prototype = {
     mutate: function (mutation) {
       if (mutation.type == "attributes") {
-        self.onAttributeChange(mutation.attributeName, self.attributes[mutation.attributeName]);
+        this.onAttributeChange(mutation.attributeName, this.element.attributes[mutation.attributeName]);
       }
     },
-    onAttributeChange: function(name, newVal) {
-      console.log(name, newVal);
-    }
-  }
-
-  function Lightbox(element) {
-    View.call(this, element);
-
-    // Register lightbox with element
-    // Alter Lightbox.for() if you change this
-    element.lightbox = this;
-
-    // Fire a source change to start the state machine
-    this.onSrcChange();
-  }
-  Lightbox.prototype = Object.create(View.prototype);
-
-  // Find any nodes with a zip-lightbox attribute
-  // and attach a Lightbox if it doesn't already
-  // have one
-  Lightbox.attach = function (node) {
-    node = node || document.body;
-    var targets = node.querySelectorAll('[zip-lightbox]');
-    for (var i = 0; i < targets.length; i++) {
-      var target = targets[i];
-      if (Lightbox.for(target)) continue;
-      new Lightbox(target);
-    }
+    onAttributeChange: function(name, newVal) {},
   }
 
   // Return a lightbox for a node, if there is one
   // This abstraction exits so we don't have to rely
   // on putting instances on element nodes later.
-  Lightbox.for = function (node) {
-    return node.lightbox;
+  View.for = function (element) {
+    return element.view;
   }
 
-  Lightbox.prototype.onAttributeChange = function (name, newVal) {
-    if (name == "src") {
-      return this.onSrcChange();
+  // Make a View usable as an element.
+  // This provides an "attach" method.
+  function addAttacher(view, name) {
+    if (!(view instanceof Function && typeof name == "string")) {
+      throw new Error("Missing or incorrect arguments for addAttacher()");
     }
+    var attacher = function (node) {
+      var target = "" + prefix + name;
+      node = node || document.body;
+      var targets = node.querySelectorAll('[' + target + ']');
+      for (var i = 0; i < targets.length; i++) {
+        var target = targets[i];
+        if (View.for(target)) continue;
+        new view(target);
+      }
+    }
+    view.attach = attacher;
   }
 
-  Lightbox.prototype.onSrcChange = function () {
-    var src = this.element.getAttribute("src")
-    log("onSrcChange", src);
+  function Lightbox(element) {
+    View.call(this, element);
   }
-
-  Lightbox.prototype.setSrc = function (src) {
-    this.element.setAttribute("src", src);
-  }
+  Lightbox.prototype = Object.create(View.prototype);
+  addAttacher(Lightbox, "lightbox");
 
   // LoadingView stuff
-  function LoadingView() {}
-  LoadingView.prototype = Object.create({});
-
-
+  function LoadingView(element) {
+    View.call(this, element)
+  }
+  LoadingView.prototype = Object.create(View.prototype);
+  addAttacher(LoadingView, "loading");
 
   return Lightbox;
 })();
