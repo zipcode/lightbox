59ba68c967dd7fdb8ee3e7a986022375aade31b4
@zip <->
2015-05-23 21:48:36 -0700

    Working LoadingView

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -7,6 +7,12 @@ window.Lightbox = (function () {
     if (!(this instanceof View)) {
       throw new Error("Called a View() or subclass without new");
     }
+
+    if (element === undefined) {
+      // Create an element
+      element = document.createElement("div");
+    }
+
     var self = this;
     this.element = element;
     element.view = this;
@@ -24,7 +30,7 @@ window.Lightbox = (function () {
   View.prototype = {
     mutate: function (mutation) {
       if (mutation.type == "attributes") {
-        this.onAttributeChange(mutation.attributeName, this.element.attributes[mutation.attributeName]);
+        this.onAttributeChange(mutation.attributeName, this.element.attributes[mutation.attributeName].value);
       }
     },
     onAttributeChange: function(name, newVal) {},
@@ -39,14 +45,78 @@ window.Lightbox = (function () {
 
   function Lightbox(element) {
     View.call(this, element);
+    this.loadSrc(this.element.attributes["src"].value);
   }
   Lightbox.prototype = Object.create(View.prototype);
 
+  Lightbox.prototype.onAttributeChange = function(name, newVal) {
+    if (this.handlers[name] instanceof Function) {
+      return this.handlers[name].call(this, newVal);
+    }
+  };
+
+  // Associate each state with a view and display that view on state change
+  Lightbox.prototype.setState = function (state, data) {
+    this.state = state;
+    var view = ({
+      "failure": FailureView,
+      "loading": LoadingView,
+    })[state];
+    if (this.view !== undefined) {
+      this.view.element.remove();
+    }
+    if (view instanceof Function) {
+      this.view = new view(undefined, data);
+      this.element.appendChild(this.view.element);
+    } else {
+      console.log("No view for ", state);
+    }
+  };
+
+  Lightbox.prototype.loadSrc = function (src) {
+    var self = this;
+    this.setState("loading");
+    var timeout = window.setTimeout(function () {
+      self.setState("failure", "Timeout");
+    }, 10000);
+    var success = function () {
+      window.cancelTimeout(timeout);
+      window.setState("success");
+    };
+    var failure = function (reason) {
+      window.cancelTimeout(timeout);
+      self.setState("failure", "Failure: " + reason);
+    };
+  };
+
+  Lightbox.prototype.handlers = {
+    src: function (src) {
+      this.loadSrc(src);
+    },
+  }
+
   // LoadingView stuff
   function LoadingView(element) {
-    View.call(this, element)
+    View.call(this, element);
+
+    for (var i = 0; i < 3; i++) {
+      var spot = document.createElement("span");
+      spot.innerHTML = "&#9679;";
+      this.element.appendChild(spot);
+    }
+
+    this.element.setAttribute("class", "zip-lightbox-loading");
   }
   LoadingView.prototype = Object.create(View.prototype);
+
+  // FailureView stuff
+  function FailureView(element, reason) {
+    View.call(this, element);
+
+    this.element.innerText = reason;
+    this.element.setAttribute("class", "zip-lightbox-failure");
+  }
+  FailureView.prototype = Object.create(View.prototype);
 
   return Lightbox;
 })();
